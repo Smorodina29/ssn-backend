003f2309e25739d84ee8d9672369825346b53ea5
darya <darya.smorodina@ddgcorp.ru>
2018-06-15 15:57:34 +0300

    Initial commit

---
+++ src/main/java/com/example/ssn/SsnApplication.java
@@ -0,0 +1,12 @@
+package com.example.ssn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SsnApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SsnApplication.class, args);
+    }
+}
