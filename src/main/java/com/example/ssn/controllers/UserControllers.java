6a36599e934fe16ffb0fe59d6f2848210fe12e88
darya <darya.smorodina@ddgcorp.ru>
2018-06-25 18:28:47 +0300

    [+]added ApiResponseactory [+] update UserService, User, UserController

--- src/main/java/com/example/ssn/controllers/UserControllers.java
+++ src/main/java/com/example/ssn/controllers/UserControllers.java
@@ -1,19 +1,18 @@
 package com.example.ssn.controllers;
 
-import com.example.ssn.User;
+import com.example.ssn.APIResponse;
+import com.example.ssn.ApiResponceFactory;
+import com.example.ssn.contracts.User;
 import com.example.ssn.repositories.UserRepositories;
 import com.example.ssn.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.RequestEntity;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @RestController
@@ -24,8 +23,16 @@ public class UserControllers {
     UserService userService;
 
     @RequestMapping(value = "/users", method = RequestMethod.GET)
+    public APIResponse getUsers(){
+               return ApiResponceFactory.success(userService.findAllUsers());
+    }
+
+    /*@RequestMapping(value = "/users", method = RequestMethod.GET)
     public ResponseEntity<List<User>> getUsers(Model model){
-       //model.addAttribute("users", userRepositories.findAll());
+        //model.addAttribute("users", userRepositories.findAll());
         return new ResponseEntity<>(userService.findAllUsers(), HttpStatus.OK);
     }
+
+    @RequestMapping(value = "/registration", method = RequestMethod.POST)
+    public */
 }
